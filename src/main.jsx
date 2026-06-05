69923cea9fc83f54222b65848121339366ff3d10
Mohamad <muhammad.molaee@gmailc.com>
2026-06-05 09:55:06 +0330

    feat(main): wrap app with TaskProvider context

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { TaskProvider } from './context/TaskContext'
 import './index.css'
-import App from './App.jsx'
+import App from './App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <TaskProvider>
+      <App />
+    </TaskProvider>
+  </StrictMode>
 )
